e57f60986aaf948c624327a0c9673e4e97bdd626
Kokc80 <kokc@inbox.ru>
2025-10-10 07:19:30 +0900

    generators

---
+++ src/generators.py
@@ -0,0 +1,115 @@
+trans_list = (
+    [
+        {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount":
+            {
+            "amount": "9824.07",
+            "currency":
+                {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+          "description": "Перевод организации",
+          "from": "Счет 75106830613657916952",
+          "to": "Счет 11776614605963066702"
+        },
+        {
+         "id": 142264268,
+         "state": "EXECUTED",
+         "date": "2019-04-04T23:20:05.206878",
+         "operationAmount":
+             {
+             "amount": "79114.93",
+             "currency":
+                 {
+                 "name": "USD",
+                 "code": "USD"
+                 }
+             },
+         "description": "Перевод со счета на счет",
+         "from": "Счет 19708645243227258542",
+         "to": "Счет 75651667383060284188"
+         },
+        {
+            "id": 1122000000,
+            "state": "EXECUTED",
+            "date": "2025-04-04T23:20:05.206878",
+            "operationAmount":
+                {
+                    "amount": "79114.93",
+                    "currency":
+                        {
+                            "name": "RUR",
+                            "code": "RUR"
+                        }
+                },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 454545400000,
+            "state": "EXECUTED",
+            "date": "2025-04-04T23:20:05.206878",
+            "operationAmount":
+                {
+                    "amount": "79114.93",
+                    "currency":
+                        {
+                            "name": "EUR",
+                            "code": "EUR"
+                        }
+                },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
+    ]
+)
+
+
+def filter_by_currency(transact: list, currency: str):
+    """принимает на вход список словарей, представляющих транзакции.
+    Функция должна возвращать итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной (например, USD)."""
+    if not transact:
+        return "Операций с такой валютой нет."
+    else:
+        for trans in transact:
+            if trans["operationAmount"]["currency"]["name"] == currency:
+                yield trans #итерация
+
+
+# #пример из условий задачи
+# usd_transactions = filter_by_currency(trans_list, "USD")
+# for _ in range(2):
+#     print(next(usd_transactions))
+
+
+def transaction_descriptions(transact_1: list):
+    """генератор принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
+    if not transact_1:
+        return "Транзакций нет"
+    else:
+        for trans_1 in transact_1:
+            yield trans_1["description"]
+
+
+# #пример из условий задачи
+# descriptions = transaction_descriptions(trans_list)
+# for _ in range(3):
+#     print(next(descriptions))
+
+
+def card_number_generator(num_1: int, num_2: int):
+    """Генератор генерирует номера карт"""
+    for num in range(num_1,num_2+1):
+        yield '{:04d} {:04d} {:04d} {:04d}'.format(num // 10**12, (num // 10**8) % 10**4, (num // 10**4) % 10**4, num % 10**4)
+
+# #пример из условий задачи
+# for card_number in card_number_generator(100001, 100009):
+#     print(card_number)
