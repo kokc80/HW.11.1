118acc7919235a5f6087bdd77e3427df6d01f841
Kokc80 <kokc@inbox.ru>
2025-10-16 05:34:47 +0900

    исправление ошибок от 16.10.2025

--- src/processing.py
+++ src/processing.py
@@ -2,13 +2,6 @@ from typing import Dict, List
 from datetime import datetime
 from typing import List
 
-def filter_by_state(banking_operations: List[Dict[str, str]], state: str = "EXECUTED") -> List:
-    filtered_list: List = []
-    for dict_item in banking_operations:
-        if dict_item.get("state") == state:
-            filtered_list.append(dict_item)
-    return filtered_list
-
 
 def sort_by_date(data_list: List[dict], reverse1: bool = True) -> List[dict]:
     """Функия принимает список словарей и необязательный параметр,
@@ -17,9 +10,17 @@ def sort_by_date(data_list: List[dict], reverse1: bool = True) -> List[dict]:
     # Преобразуем строки дат в объекты datetime для корректной сортировки
     list_sorted = sorted(
         data_list,
-        key=lambda x: datetime.fromisoformat(x["date"].replace('T', '+00:00')),
+        key=lambda x: datetime.fromisoformat(
+            x["date"].replace('Z', '+00:00')),
         reverse=reverse1
     )
     return list_sorted
 
 
+def filter_by_state(banking_operations: List[Dict[str, str]],
+                    state: str = "EXECUTED") -> List:
+    filtered_list: List = []
+    for dict_item in banking_operations:
+        if dict_item.get("state") == state:
+            filtered_list.append(dict_item)
+    return filtered_list
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,4 @@
 import pytest
-
 from src.processing import *
 
 
@@ -13,65 +12,64 @@ def pytest_list_1():
     ]
 
 
+@pytest.fixture
+def test_list_sorted():
+    return [
+     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+     {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}
+ ]
+
+
 def test_filter_by_state_1(pytest_list_1):
     assert filter_by_state(pytest_list_1) == [
-        {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+         {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
     ]
 
 
-def test_filter_by_state_2(pytest_list_1):
-    assert filter_by_state(pytest_list_1) == [
-        {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
-    ]
-
-
-def test_list_sorted():
-    test_list_sorted = [
-        {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
-    ]
-    return test_list_sorted
-
-
-def test_sort_by_date_1():
-    assert sort_by_date(
-        [
-            {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],
-            False
-    ) == [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]
+def test_sort_by_date_1(test_list_sorted):
+     assert sort_by_date(
+ [
+             {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],
+             False
+     ) == test_list_sorted
 
 
 pytest_param1 = [
-   [{"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}],True]
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+]
+
 pytest_param2 = [
-    [{"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],True]
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+]
+
 pytest_param3 = [
-    [{"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],True]
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+]
 
 
-
-@pytest.mark.parametrize("data_list,expected_result", [(pytest_param1, test_list_sorted),
-                                                       (pytest_param2, test_list_sorted),
-                                                       (pytest_param3, test_list_sorted)]
+@pytest.mark.parametrize(
+    "data_list,expected_result",
+    [
+        (pytest_param1, test_list_sorted),
+        (pytest_param2, test_list_sorted),
+        (pytest_param3, test_list_sorted)
+    ]
 )
-def test_sort_by_date_2 (data_list, expected_result):
-    assert sort_by_date(data_list) == expected_result
+def test_sort_by_date_2(data_list, expected_result):
+    assert sort_by_date(data_list, True) == [expected_result]
+
