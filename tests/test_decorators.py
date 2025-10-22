60af0e78c17c5b89b2792961f1875a4c3a749a8c
Kokc80 <kokc@inbox.ru>
2025-10-23 06:13:20 +0900

    Добавлен декоратор тест декоратора

---
+++ tests/test_decorators.py
@@ -0,0 +1,11 @@
+from src.decorators import *
+def test_log_file(capsys):
+    @log()
+    def my_function(x, y):
+        return x / y
+
+    my_function(20, 0)
+    result = capsys.readouterr()
+    assert (
+        result.out == 'Ошибка в функции: my_function Ошибка: division by zero. Аргументы: (20, 0), {}'
+    )
