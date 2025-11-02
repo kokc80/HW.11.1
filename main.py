996e267934f566bb5579b092e1c695c81dba4f5e
Kokc80 <kokc@inbox.ru>
2025-11-02 21:11:29 +0900

    {'message': 'No API key found in request'}

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ transaction_dict=[]
 file_path_data = f"{ROOT_DIR}\\data\\operations.json"
 # print(f"Путь {file_path_data}")
 transaction_dict = read_json(file_path_data)
-try:
-    # print(f"tran list {transaction_dict}")
-    tr_list = transaction_dict[0]
-    if tr_list["operationAmount"]["currency"]["code"] == "RUB":
-        print("RUB")
-        print(tr_list["operationAmount"]["amount"])
-    else:
-        print(convert_exchange_rate(tr_list["operationAmount"]["currency"]["code"], "RUB",
+
+
+tr_list = transaction_dict[2]
+if tr_list["operationAmount"]["currency"]["code"] == "RUB":
+    print("RUB")
+    print(tr_list["operationAmount"]["amount"])
+else:
+    print(convert_exchange_rate(tr_list["operationAmount"]["currency"]["code"], "RUB",
                                   float(tr_list["operationAmount"]["amount"]), date_now.strftime("%Y-%m-%d")))
-except:
-    print('Непредвиденная ошибка')
--- src/external_api.py
+++ src/external_api.py
@@ -13,13 +13,16 @@ def convert_exchange_rate(money_from: str, money_to: str, money_amount: float, d
     """Функция для работы с API формирует ссылку для работы с API и конвертирует валюту на выходе сумма в рублях"""
     url = (f"https://api.apilayer.com/exchangerates_data/convert?to="
            f"{money_to}&from={money_from}&amount={money_amount}")
-    # print(f"URL {url}")
-    # print(api_key)
+    print(f"URL {url}")
+    print(api_key)
     payload = {}
     headers = {
         "apikey": api_key
     }
     response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.get(url)
+    data = response.json()
+    print(data)
     # status_code = response.status_code
     result = response.json()["result"]
     return result
