0d3aec0f474413b70d572a8607c5b3057d98654c
Kokc80 <kokc@inbox.ru>
2025-11-22 04:44:21 +0900

    22.11.2025 восстановил тесты test_proccessing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,7 @@
 import pytest
+from unicodedata import category
 
-from src.processing import filter_by_state, sort_by_date
+from src.processing import filter_by_state, sort_by_date, process_bank_operations
 
 
 @pytest.fixture
@@ -74,6 +75,67 @@ pytest_param3 = [
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
 ]
 
+test1 = [
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  },
+  {
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  },
+  {
+    "id": 939719570,
+    "state": "EXECUTED",
+    "date": "2018-06-30T02:08:58.425572",
+    "operationAmount": {
+      "amount": "9824.07",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 75106830613657916952",
+    "to": "Счет 11776614605963066702"
+  },
+  {
+    "id": 587085106,
+    "state": "PENDING",
+    "date": "2018-03-23T10:45:06.972075",
+    "operationAmount": {
+      "amount": "48223.05",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 41421565395219882431"
+  },
+]
 
 @pytest.mark.parametrize(
     "data_list,expected_result",
@@ -81,3 +143,7 @@ pytest_param3 = [
 )
 def test_sort_by_date_2(data_list, expected_result):
     assert list(sort_by_date(data_list, True)) == expected_result
+
+def test_process_bank_operations():
+    categories_list=({'Перевод организации', 'Открытие вклада'})
+    assert process_bank_operations(test1,categories_list) == ({'Перевод организации': 3, 'Открытие вклада': 1})
