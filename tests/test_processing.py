e701478ee67c90ae58988ac7e07d1990832f8a68
Kokc80 <kokc@inbox.ru>
2025-11-22 22:38:23 +0900

    22.11.2025 ошибка с пробелами, решена Ctrl+ALt+L (!!! непонятно)

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,5 +1,4 @@
 import pytest
-from unicodedata import category
 
 from src.processing import filter_by_state, sort_by_date, process_bank_operations, process_bank_search
 
@@ -41,16 +40,16 @@ def test_filter_by_state_1(pytest_list_1):
 
 def test_sort_by_date_1(test_list_sorted_f):
     assert (
-        sort_by_date(
-            [
-                {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            ],
-            False,
-        )
-        == test_list_sorted_f
+            sort_by_date(
+                [
+                    {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                ],
+                False,
+            )
+            == test_list_sorted_f
     )
 
 
@@ -76,66 +75,68 @@ pytest_param3 = [
 ]
 
 test1 = [
-  {
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
+    {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount":
+            {"amount": "31957.58",
+             "currency":
+                 {
+                     "name": "руб.",
+                     "code": "RUB"
+                 }
+             },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
     },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  },
-  {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
+    {
+        "id": 41428829,
+        "state": "EXECUTED",
+        "date": "2019-07-03T18:35:29.512364",
+        "operationAmount": {
+            "amount": "8221.37",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "MasterCard 7158300734726758",
+        "to": "Счет 35383033474447895560"
     },
-    "description": "Перевод организации",
-    "from": "MasterCard 7158300734726758",
-    "to": "Счет 35383033474447895560"
-  },
-  {
-    "id": 939719570,
-    "state": "EXECUTED",
-    "date": "2018-06-30T02:08:58.425572",
-    "operationAmount": {
-      "amount": "9824.07",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
     },
-    "description": "Перевод организации",
-    "from": "Счет 75106830613657916952",
-    "to": "Счет 11776614605963066702"
-  },
-  {
-    "id": 587085106,
-    "state": "PENDING",
-    "date": "2018-03-23T10:45:06.972075",
-    "operationAmount": {
-      "amount": "48223.05",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
+    {
+        "id": 587085106,
+        "state": "PENDING",
+        "date": "2018-03-23T10:45:06.972075",
+        "operationAmount": {
+            "amount": "48223.05",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 41421565395219882431"
     },
-    "description": "Открытие вклада",
-    "to": "Счет 41421565395219882431"
-  },
 ]
+
 
 @pytest.mark.parametrize(
     "data_list,expected_result",
@@ -144,23 +145,24 @@ test1 = [
 def test_sort_by_date_2(data_list, expected_result):
     assert list(sort_by_date(data_list, True)) == expected_result
 
+
 def test_process_bank_operations():
-    categories_list=({'Перевод организации', 'Открытие вклада'})
-    assert process_bank_operations(test1,categories_list) == ({'Перевод организации': 3, 'Открытие вклада': 1})
+    categories_list = ({'Перевод организации', 'Открытие вклада'})
+    assert process_bank_operations(test1, categories_list) == ({'Перевод организации': 3, 'Открытие вклада': 1})
 
 
 def test_process_bank_search():
-    assert process_bank_search(test1,"Открытие") == [{
-    "id": 587085106,
-    "state": "PENDING",
-    "date": "2018-03-23T10:45:06.972075",
-    "operationAmount": {
-      "amount": "48223.05",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Открытие вклада",
-    "to": "Счет 41421565395219882431"
-  }]
+    assert process_bank_search(test1, "Открытие") == [{
+        "id": 587085106,
+        "state": "PENDING",
+        "date": "2018-03-23T10:45:06.972075",
+        "operationAmount": {
+            "amount": "48223.05",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 41421565395219882431"
+    }]
