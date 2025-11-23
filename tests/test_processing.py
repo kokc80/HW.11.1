147efbc6c7df5cb9c9628ca6001d369718b00c1b
Kokc80 <kokc@inbox.ru>
2025-11-23 09:39:00 +0900

    23.11.2025 Ошибка переноса строк не могу понять в чем причина .\tests\test_processing.py:48:30: E126 continuation line over-indented for hanging inden

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -40,16 +40,14 @@ def test_filter_by_state_1(pytest_list_1):
 
 def test_sort_by_date_1(test_list_sorted_f):
     assert (
-            sort_by_date(
-                [
-                    {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-                    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                ],
-                False,
-            )
-            == test_list_sorted_f
+        sort_by_date([
+            {"id": 414288291, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                             ],
+            False,
+        ) == test_list_sorted_f
     )
 
 
